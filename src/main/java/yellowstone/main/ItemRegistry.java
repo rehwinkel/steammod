450bca8c537f87e0da47e1ecc4aa53d66734ebe2
Igli2 <sg.gerdon@gmx.de>
2020-07-23 17:03:01 +0200

    minor changes

--- src/main/java/yellowstone/main/ItemRegistry.java
+++ src/main/java/yellowstone/main/ItemRegistry.java
@@ -1,6 +1,7 @@
 package yellowstone.main;
 
 import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
@@ -10,5 +11,6 @@ public class ItemRegistry {
             .create(ForgeRegistries.ITEMS, Yellowstone.MODID);
 
     // public static final RegistryObject<Item> GOOSEBERRIES = ITEMS.register("gooseberries", () -> new Item(null));
+    public static final RegistryObject<Item> BRASS_INGOT = ITEMS.register("brass_ingot", () -> new Item(new Item.Properties()));
 
 }
