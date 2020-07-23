f9ef8264611086d2afa32f3fd26c67af0bc375c7
Ian Rehwinkel <ian.rehwinkel@tutanota.com>
2020-07-23 11:10:45 +0200

    moved stuff in BlockRegistry

--- src/main/java/yellowstone/main/BlockRegistry.java
+++ src/main/java/yellowstone/main/BlockRegistry.java
@@ -16,6 +16,9 @@ public class BlockRegistry {
     public static final RegistryObject<Block> DIRT = BLOCKS.register("dirt", () -> new Block(
             AbstractBlock.Properties.create(Material.EARTH, MaterialColor.DIRT).hardnessAndResistance(0.5F)
                     .sound(SoundType.GROUND)));
+    public static final RegistryObject<Block> STONE = BLOCKS.register("stone", () -> new Block(
+            Block.Properties.create(Material.ROCK, MaterialColor.STONE).hardnessAndResistance(1.5F, 6.0F)
+                    .sound(SoundType.STONE)));
     public static final RegistryObject<Block> DOUGLAS_PLANKS = BLOCKS.register("douglas_planks", () -> new Block(
             AbstractBlock.Properties.create(Material.WOOD, MaterialColor.GRAY).hardnessAndResistance(2.0F, 3.0F)
                     .sound(SoundType.WOOD)));
@@ -36,17 +39,12 @@ public class BlockRegistry {
     public static final RegistryObject<Block> DOUGLAS_SLAB = BLOCKS.register("douglas_slab", () -> new SlabBlock(
             AbstractBlock.Properties.create(Material.WOOD, MaterialColor.GRAY).hardnessAndResistance(2.0F, 3.0F)
                     .sound(SoundType.WOOD)));
-
-    public static final RegistryObject<Block> STONE = BLOCKS.register("stone", () -> new Block(
-            Block.Properties.create(Material.ROCK, MaterialColor.STONE).hardnessAndResistance(1.5F, 6.0F)
-                    .sound(SoundType.STONE)));
-
-    public static final RegistryObject<Block> LEAVES = BLOCKS.register("leaves", () -> new LeavesBlock(
+    public static final RegistryObject<Block> DOUGLAS_LEAVES = BLOCKS.register("douglas_leaves", () -> new LeavesBlock(
             Block.Properties.create(Material.LEAVES, MaterialColor.GREEN).hardnessAndResistance(0.2F)
                     .sound(SoundType.PLANT)));
 
     static {
-        ITEMS.register("dirt", () -> new BlockItem(DIRT.get(), new Item.Properties()));
+        ITEMS.register("dirt", () -> new BlockItem(DIRT.get(), new Item.Properties().group(YellowStone.y_blocks)));
         ITEMS.register("douglas_fence",
                 () -> new BlockItem(DOUGLAS_FENCE.get(), new Item.Properties().group(YellowStone.y_blocks)));
         ITEMS.register("douglas_fence_gate",
@@ -57,8 +55,8 @@ public class BlockRegistry {
                 () -> new BlockItem(DOUGLAS_STAIRS.get(), new Item.Properties().group(YellowStone.y_blocks)));
         ITEMS.register("douglas_slab",
                 () -> new BlockItem(DOUGLAS_SLAB.get(), new Item.Properties().group(YellowStone.y_blocks)));
-        ITEMS.register("stone", () -> new BlockItem(STONE.get(), new Item.Properties()));
-        ITEMS.register("leaves", () -> new BlockItem(LEAVES.get(), new Item.Properties()));
+        ITEMS.register("stone", () -> new BlockItem(STONE.get(), new Item.Properties().group(YellowStone.y_blocks)));
+        ITEMS.register("douglas_leaves", () -> new BlockItem(DOUGLAS_LEAVES.get(), new Item.Properties().group(YellowStone.y_blocks)));
     }
 
 }
